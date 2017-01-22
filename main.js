a1f384b69eea54f3587c848890db47dceb3423a6
Tony Wu <tony.wu@indexexchange.com>
2017-01-22 17:05:59 -0500

    removed commented code

--- main.js
+++ main.js
@@ -2,18 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
-
-
 ReactDOM.render(<App />, document.getElementById('app'));
-
-// const buttonsInstance = (
-//   <ButtonToolbar>
-//     <Button bsStyle="primary" bsSize="large" active>Primary button</Button>
-//     <Button bsSize="large" active>Button</Button>
-//   </ButtonToolbar>
-// );
-
-// ReactDOM.render(buttonsInstance, document.getElementById('app'));
 
 
 
